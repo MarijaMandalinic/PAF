cd64fff13b2e350ac85ac60ec39576ab42877f20
MarijaMandalinic <mmandalin@pmfst.hr>
2023-03-13 08:43:39 +0100

    zad 3

--- Vjezbe/Vjezbe_1/zadatak_3.py
+++ Vjezbe/Vjezbe_1/zadatak_3.py
@@ -15,6 +15,9 @@ y1=int(y1)
 x2=int(x2)
 y2=int(y2)
 k=(y2-y1)/(x2-x1)
-l=x1*k+y1
-print("y={}x+{}".format(k,l))
+l=-x1*k+y1
+if l<0:
+    print("y={}x-{}".format(k,abs(l)))
+else:
+    print("y={}x+{}".format(k,l))
 
