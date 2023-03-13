7d56ad6d4b5caebe9bb014fd7429c19796f4b205
MarijaMandalinic <mmandalin@pmfst.hr>
2023-03-13 08:45:45 +0100

    zadatak 4

--- Vjezbe/Vjezbe_1/zadatak_4.py
+++ Vjezbe/Vjezbe_1/zadatak_4.py
@@ -1,6 +1,9 @@
 def funkcija(x1,y1,x2,y2):
     k=(y2-y1)/(x2-x1)
-    l=x1*k+y1
-    print("y={}x+{}".format(k,l))
-funkcija(2,3,4,5)
+    l=-x1*k+y1
+    if l<0:
+        print("y={}x-{}".format(k,abs(l)))
+    else:
+        print("y={}x+{}".format(k,l))
+funkcija(2,6,4,5)
 
