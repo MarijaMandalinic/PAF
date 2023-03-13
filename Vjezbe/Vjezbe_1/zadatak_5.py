634ba6713c41d4a2a8e306fc547d82e9a86d9ea9
MarijaMandalinic <mmandalin@pmfst.hr>
2023-03-13 08:46:57 +0100

    zad 5

--- Vjezbe/Vjezbe_1/zadatak_5.py
+++ Vjezbe/Vjezbe_1/zadatak_5.py
@@ -5,8 +5,10 @@ def funkcija(x1,y1,x2,y2, ime, a): #fja za pravac kroz 2 tocke, ime pod kojim ce
     l=-x1*k+y1
     x=np.linspace(0,9,10)
     y=k*x+l
-    print(x,y)
-    print("y={}x+{}".format(k,l))
+    if l<0:
+        print("y={}x-{}".format(k,abs(l)))
+    else:
+        print("y={}x+{}".format(k,l))
     plt.plot(y)
     plt.scatter([x1], [y1], color="red", zorder=5) #tocka1 na grafu
     plt.scatter([x2], [y2], color="red", zorder=5) #tocka2
@@ -14,4 +16,4 @@ def funkcija(x1,y1,x2,y2, ime, a): #fja za pravac kroz 2 tocke, ime pod kojim ce
         plt.show()
     else:
         plt.savefig(ime+".pdf", format='pdf')
-funkcija(2,6,4,5, "slika", 0)
+funkcija(2,3,4,5, "slika", 1)
