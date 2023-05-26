f31b052eb1ab36d1dc17525ae239240db5cf2f26
MarijaMandalinic <mmandalin@pmfst.hr>
2023-05-26 13:39:16 +0200

    1. zad

--- Vjezbe/Vjezbe_8/zadatak_1.py
+++ Vjezbe/Vjezbe_8/zadatak_1.py
@@ -4,14 +4,21 @@ import matplotlib.pyplot as plt
 
 #elektron
 
-e1 = em.em_polje(20, 1, -1, 0, 0, 0, 0, 0, 1, 0.1, 0.1, 0.1, 0.01)  #t,  m, q, ex, ey, ez, bx, by, bz, vx, vy, vz, dt
+e1 = em.em_polje(20, 1, -1, np.array((0,0,0)), np.array((0,0,1)), np.array((0.1,0.1,0.1)), 0.01)  #t,  m, q, e, b, v, dt
 e1.gibanje()
-plt.title('Putanja nabijene čestice - elektron')
-plt.show()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(e1.listax, e1.listay, e1.listaz, label= "elektron")
 
 #pozitron
 
-e2 = em.em_polje(20, 1, 1, 0, 0, 0, 0, 0, 1, 0.1, 0.1, 0.1, 0.01)  #t,  m, q, ex, ey, ez, bx, by, bz, vx, vy, vz, dt
+e2 = em.em_polje(20, 1, 1, np.array((0,0,0)), np.array((0,0,1)), np.array((0.1,0.1,0.1)), 0.01) 
 e2.gibanje()
-plt.title('Putanja nabijene čestice - pozitron')
+ax.plot(e2.listax, e2.listay, e2.listaz, label = "proton")
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.title('Putanja nabijene čestice')
+leg = plt.legend(loc='upper right')
 plt.show()
