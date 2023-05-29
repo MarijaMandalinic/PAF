4f4fceab3385b1b8271da0da4c5d5855fa520648
MarijaMandalinic <mmandalin@pmfst.hr>
2023-05-29 08:29:46 +0200

    interakcija zemlja sunce

--- Vjezbe/Vjezbe_9/zemlja_sunce.py
+++ Vjezbe/Vjezbe_9/zemlja_sunce.py
@@ -4,7 +4,7 @@ import numpy as np
 
 t1 = inter.interakcija(5.9742*10**(24), 1.989*10**(30),np.array((1.486*10**(11), 0)), np.array((0,0)),np.array((0,29783)),np.array((0,0)), 365.242*24*60*60)
 t1.gibanje()
-#m1, m2, r1,r2, v1, v2, t, dt=0.5
+#m1, m2, r1,r2, v1, v2, t, dt = 60*60*12
 x1, x2, y1, y2 = t1.gibanje()
 plt.scatter(x1, y1, s = 0.5, label = 'zemlja')
 plt.scatter(x2, y2, c = 'y', s = 10, label = 'sunce')
